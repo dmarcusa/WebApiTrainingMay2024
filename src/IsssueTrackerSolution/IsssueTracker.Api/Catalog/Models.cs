2de574fb5dcfbed9191d4afa2f510ddc2c305a30
Dana Marcusanu <dmarcusanu@gmail.com>
2024-05-06 12:31:27 -0400

    before lunch

---
+++ src/IsssueTrackerSolution/IsssueTracker.Api/Catalog/Models.cs
@@ -0,0 +1,7 @@
+﻿namespace IsssueTracker.Api.Catalog;
+
+//"Models" are the things that leave or arrive from outside our service boundary
+//they are C# types that weill be desearilize or serialize from json coming ontp our out of api
+public record CreateCatalogItemRequest(string Title, string Description);
+
+public record CatalogItemResponse(Guid id, string Title, string Description);
